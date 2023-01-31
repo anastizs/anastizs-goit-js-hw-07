2299616002d563fd38b30c9610f83e8db9a57a81
Anastasiia <dessertkiev@gmail.com>
2023-01-31 23:09:24 +0200

    Update 02-lightbox.js

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,41 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
 console.log(galleryItems);
+
+const imgContainer = document.querySelector(".gallery");
+
+const cardsMarkup = createColorCardsMarkup(galleryItems);
+
+imgContainer.insertAdjacentHTML("beforeend", cardsMarkup);
+
+imgContainer.addEventListener("click", onImgContainerClick);
+
+function createColorCardsMarkup(galleryItems) {
+  return galleryItems
+    .map(({ preview, original, description }) => {
+      return `
+      <a class="gallery__item" href="${original}">
+    <img
+      class="gallery__image"
+      src="${preview}"
+      alt="${description}"
+    />
+  </a>
+    `;
+    })
+    .join("");
+}
+function onImgContainerClick(e) {
+  e.preventDefault();
+  const isImgEl = e.target.classList.contains("gallery__image");
+
+  if (!isImgEl) {
+    return;
+  }
+  const lightbox = new SimpleLightbox(".gallery a", {
+    captionsData: "alt",
+    captionDelay: 250,
+    // navText: ["&larr;", "&rarr;"],
+  });
+}
